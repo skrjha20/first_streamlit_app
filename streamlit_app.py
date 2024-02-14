0fde173e229851b366f164d81057cfeac0d5edde
Sanjeev Kr. Jha <skrjha20@gmail.com>
2024-02-14 20:24:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oat Meal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
