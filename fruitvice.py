ca855e4a1b49b12c80a27e01256ea53dc6b98ca7
Sanjeev Kr. Jha <skrjha20@gmail.com>
2024-02-18 21:44:26 +0530

    Update fruitvice.py

--- fruitvice.py
+++ fruitvice.py
@@ -52,7 +52,7 @@ def insert_row_snowflake(new_fruit, my_cnx):
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  back_from_function = insert_row_snowflake(new_fruit, my_cnx)
+  back_from_function = insert_row_snowflake(add_my_fruit, my_cnx)
   streamlit.text(back_from_function)
   
 #streamlit.header("Build Your Own Fruit Smoothie")
