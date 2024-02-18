5f969a8a47304d9238cd162ecc25484a862c391f
Sanjeev Kr. Jha <skrjha20@gmail.com>
2024-02-18 20:19:10 +0530

    Update fruitvice.py

--- fruitvice.py
+++ fruitvice.py
@@ -34,3 +34,7 @@ my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
 #streamlit.text("Hello from Snowflake:")
 streamlit.dataframe(my_data_row)
+
+#streamlit.write('What fruit would you like to add?')
+fruit_choice1 = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice1)
