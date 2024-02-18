c9d1c28fcabdfeb31c9961f5aadab1d4a524202a
Sanjeev Kr. Jha <skrjha20@gmail.com>
2024-02-18 20:31:13 +0530

    Update fruitvice.py

--- fruitvice.py
+++ fruitvice.py
@@ -38,3 +38,6 @@ streamlit.dataframe(my_data_row)
 #streamlit.write('What fruit would you like to add?')
 fruit_choice1 = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice1)
+
+streamlit.header("Build Your Own Fruit Smoothie")
+my_cur.execute("insert into fruit_load_list values ('from streamlit')"
