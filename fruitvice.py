a54bb0b3d3fcf33f9983a75155dc8d4972052a13
Sanjeev Kr. Jha <skrjha20@gmail.com>
2024-02-18 21:31:54 +0530

    Update fruitvice.py

--- fruitvice.py
+++ fruitvice.py
@@ -34,14 +34,23 @@ try:
 except URLError as e:
   streamlit.error()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute('select * from fruit_load_list')
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute('select * from fruit_load_list')
+    return my_cur.fetchall()
+
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_row = get_fruit_load_list()
+  streamlit.dataframe(my_data_row)
+  
+#my_cur = my_cnx.cursor()
+#my_cur.execute('select * from fruit_load_list')
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchall()
-streamlit.text("The fruit load list contains:")
+#my_data_row = my_cur.fetchall()
+#streamlit.text("The fruit load list contains:")
 #streamlit.text("Hello from Snowflake:")
-streamlit.dataframe(my_data_row)
+#streamlit.dataframe(my_data_row)
 
 #streamlit.write('What fruit would you like to add?')
 fruit_choice1 = streamlit.text_input('What fruit would you like to add?','jackfruit')
