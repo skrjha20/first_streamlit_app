2e8e39f69119a070f0449f51eb6f6b750dd93629
Sanjeev Kr. Jha <skrjha20@gmail.com>
2024-02-18 21:11:03 +0530

    Update fruitvice.py

--- fruitvice.py
+++ fruitvice.py
@@ -42,6 +42,8 @@ streamlit.write('Thanks for adding ', fruit_choice1)
 streamlit.header("Build Your Own Fruit Smoothie")
 my_cur.execute("select * from fruit_load_list")
 fruits_selected = streamlit.multiselect("Pick some fruits:", my_cur.fetchall())
-#fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(my_cur.execute("select * from fruityvice where name in fruits_selected"))
+my_cur.execute("select * from fruityvice where name in fruits_selected")
+my_data_row = my_cur.fetchall()
+streamlit.dataframe(my_data_row)
+
 
