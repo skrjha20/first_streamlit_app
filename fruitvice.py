fd190baa1d49fafb7279d54224e421e02f57d693
Sanjeev Kr. Jha <skrjha20@gmail.com>
2024-02-18 14:34:52 +0530

    Update fruitvice.py

--- fruitvice.py
+++ fruitvice.py
@@ -1,1 +1,11 @@
+import streamlit
+import pandas
 
+streamlit.title('My Mom's New Healthy Dinner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('Pick some fruits:')
